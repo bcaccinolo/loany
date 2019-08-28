9dfc2aa7d089e67e33c04dc55d8755aba8035987
Benoit Caccinolo <bcaccinolo@synbioz.com>
2019-08-28 17:01:33 +0200

    addition of prime number calculation

--- lib/loany/scoring.ex
+++ lib/loany/scoring.ex
@@ -1,14 +1,30 @@
 defmodule Loany.Scoring do
-
   def evaluate(amount) do
-    evaluate_higher_than_previous(amount)
+    with  {:ok} <- evaluate_higher_than_previous(amount),
+          {:ok} <- is_prime_number(amount)
+      do
+        {:ok, :loan_accepted}
+      else
+        {:error, response} -> {:error, response}
+    end
   end
 
   def evaluate_higher_than_previous(amount) do
     cond do
-      amount > Loany.AmountsAgent.get_highest_amount() -> {:ok, :loan_accepted}
+      amount > Loany.AmountsAgent.get_highest_amount() -> {:ok}
       true -> {:error, :loan_to_low}
     end
-
   end
+
+  def is_prime_number(amount) do
+    cond do
+      is_prime_number(amount, amount - 1) -> {:ok}
+      true -> {:error, :not_a_prime_number}
+    end
+  end
+
+  defp is_prime_number(x, _) when x <= 1, do: false
+  defp is_prime_number(_, 1), do: true
+  defp is_prime_number(x, i) when rem(x, i) == 0, do: false
+  defp is_prime_number(x, i), do: is_prime_number(x, i - 1)
 end
--- test/loany/scoring_test.exs
+++ test/loany/scoring_test.exs
@@ -1,17 +1,39 @@
 defmodule Loany.ScoringTest do
   use ExUnit.Case
 
-  describe "the amount is lower than preceding highest loan" do
-    initial_state  = [10000, 5000, 1000]
-    Loany.AmountsAgent.start_link(initial_state)
-
-    assert {:error, :loan_to_low} == Loany.Scoring.evaluate(5000)
+  setup do
+    initial_state = [10_000, 5_000, 1_000]
+    {:ok, pid} = Loany.AmountsAgent.start_link(initial_state)
+    {:ok, pid: pid}
   end
 
-  describe "the amount is higher than preceding highest loan" do
-    initial_state  = [10000, 5000, 1000]
-    Loany.AmountsAgent.start_link(initial_state)
+  describe "evaluate/1" do
+    test "amount is lower than preceding highest loan" do
+      assert {:error, :loan_to_low} == Loany.Scoring.evaluate(5_000)
+    end
 
-    assert {:ok, :loan_accepted} == Loany.Scoring.evaluate(50000)
+    test "amount is the highest amount and a prime number" do
+      assert {:ok, :loan_accepted} == Loany.Scoring.evaluate(59_981)
+    end
+  end
+
+  describe "evaluate_higher_than_previous/1" do
+    test "the amount is lower than preceding highest loan" do
+      assert {:error, :loan_to_low} == Loany.Scoring.evaluate_higher_than_previous(5_000)
+    end
+
+    test "the amount is higher than preceding highest loan" do
+      assert {:ok} == Loany.Scoring.evaluate_higher_than_previous(50_000)
+    end
+  end
+
+  describe "is_prime_number/1" do
+    test "the amount is a prime number" do
+      assert {:ok} == Loany.Scoring.is_prime_number(59_957)
+    end
+
+    test "the amount is NOT a prime number" do
+      assert {:error, :not_a_prime_number} == Loany.Scoring.is_prime_number(60_000)
+    end
   end
 end
